dbb3827f97932dc2a7e7f368d2204eb114101ec8
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-01-31 16:08:07 +0100

    nav bar fixed

--- epi-books/src/components/MyNav.jsx
+++ epi-books/src/components/MyNav.jsx
@@ -1,22 +1,22 @@
-import { Navbar, Nav } from 'react-bootstrap'
-
+import { Navbar, Nav } from "react-bootstrap";
 
 const MyNav = (props) => {
-    return(
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home" className='ml-4'>Epi-Books - {props.genre} </Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="ml-auto mr-4">
-      <Nav.Link href="#">Home</Nav.Link>
-      <Nav.Link href="#">About</Nav.Link>
-      <Nav.Link href="#">Browse</Nav.Link>
-    </Nav>
-    <Nav>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-)
-}
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+      <Navbar.Brand href="#home" className="ml-4">
+        Epi-Books - {props.genre}{" "}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto mr-4">
+          <Nav.Link href="#">Home</Nav.Link>
+          <Nav.Link href="#">About</Nav.Link>
+          <Nav.Link href="#">Browse</Nav.Link>
+        </Nav>
+        <Nav></Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-export default MyNav
+export default MyNav;
